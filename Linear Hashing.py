fb06a31c6bf70eb595fda22d506fb55b647a44ec
FlandPan <43575097+FlandPan@users.noreply.github.com>
2018-10-10 18:57:10 -0400

    First attempt at linear hashing

--- Linear Hashing.py
+++ Linear Hashing.py
@@ -1,6 +1,7 @@
+#Chose random prime numbers to multply and modulus with
 def hash(num):
     return num*47%101
-
+#Opened a file with N integers
 def create_list(filename):
     with open(filename, 'r') as file:
         size = int(file.readline())
@@ -8,7 +9,7 @@ def create_list(filename):
         for i in range(0, size):
             nums.append(int(file.readline()))
         return nums
-    
+#Hashing function
 def create_hashed(nums):
     length = len(nums)
     hashed = ['']*length
